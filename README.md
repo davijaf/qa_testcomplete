a51f8ce4e9127ea7a31c9571584a28e218349b85
davijaf <davijose@gmail.com>
2022-07-26 12:56:26 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# qa_testcomplete
+Functional tests with TestComplete
+
+## Content:
+
+* What is automated testing
+* TestComplete UI
+* TestComplete Projects
+* TestComplete Test Object Model
+* Check Points
+* Name Mapping
+* Check your knowledge and get certified
+
+TestComplete Training covers the tools: TestComplete UI, TestComplete Projects, TestComplete Test Object Model, Check Points, Name Mapping.
+
+Learned how to create simple functional tests with TestComplete fast.
---
+++ Script/Unit1.py
@@ -0,0 +1,20 @@
+﻿def Login():
+    Browsers.Item[btChrome].Navigate(Project.Variables.envURL)
+    Aliases.browser.pageShop.Wait()
+    Aliases.browser.BrowserWindow.Maximize()
+    Aliases.browser.pageShop.header.navUsd.linkLogIn.textnodeLogIn.Click()
+    Aliases.browser.pageLogin.Wait()
+    Aliases.browser.pageLogin.sectionContent.textboxUsernameoremail.Click()
+    Aliases.browser.pageLogin.sectionContent.textboxUsernameoremail.SetText("davijaf")
+    Aliases.browser.pageLogin.sectionContent.passwordboxPassword.Click()
+    Aliases.browser.pageLogin.sectionContent.passwordboxPassword.SetText(Project.Variables.Password1)
+    Aliases.browser.pageLogin.sectionContent.buttonLogIn.ClickButton()
+    
+
+def Logout():
+  Browsers.Item[btChrome].Navigate("https://services.smartbear.com/samples/TestComplete12/smartstore/")
+  browser = Aliases.browser
+  browser.BrowserWindow.Maximize()
+  link = browser.pageShop.header.navUsd
+  Aliases.browser.pageLogin.sectionContent.link.linkDavijaf.textnode.Click()
+  Aliases.browser.pageLogin.sectionContent.link.linkLogOut.textnodeLogOut.Click()
